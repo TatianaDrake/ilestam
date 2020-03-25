0b70547083a1ec52e2059cef40ad29c0e1f44f1b
TatianaDrake <tatiana.drakinkaya@gmail.com>
2020-03-25 01:46:48 +0100

    slider css
    under construction

--- myScript.js
+++ myScript.js
@@ -1,8 +1,15 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.innerHTML === "Hello") {
-    x.innerHTML = "Swapped text!";
-  } else {
-    x.innerHTML = "Hello";
-  }
-}
+
+//hide the paragraph start
+document.getElementById('button').addEventListener('click', function() {
+  let arr = document.getElementsByClassName('block_none')
+for (item of arr) {
+		item.classList.add('block_visible')
+	}
+  })
+document.getElementById('mindre').addEventListener('click', function() {
+  let arr = document.getElementsByClassName('block_none')
+for (item of arr) {
+		item.classList.remove('block_visible')
+	}
+  })
+  //hide the paragraph finish
